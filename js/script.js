aee6f9d105c0ca29b2ccadb800ad19177f7dfa90
AdaZhao1211 <adazhao1211@gmail.com>
2016-11-07 11:15:39 -0500

    update

--- js/script.js
+++ js/script.js
@@ -6,7 +6,35 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+
+
+
 $(document).ready(function(){
-  var b = $("#test");
-  b.append($(window).width());
+// Get the button that opens the modal
+var btn = $(".modalimg");
+
+// Get the <span> element that closes the modal
+var span = $(".close");
+var thismodal;
+var themodal;
+// When the user clicks the button, open the modal
+btn.click(function() {
+  thismodal = btn.attr("id");
+  thismodal += "modal"
+  themodal = $("#"+thismodal);
+  themodal.show();
+})
+
+// When the user clicks on <span> (x), close the modal
+span.click(function() {
+  themodal.hide();
+})
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target.id == thismodal) {
+        themodal.hide();
+    }
+}
 })
