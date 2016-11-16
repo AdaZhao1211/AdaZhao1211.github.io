ef363215e649eb68eeb00e5315a270fa7e049c50
AdaZhao1211 <adazhao1211@gmail.com>
2016-11-16 11:21:54 -0500

    about

--- js/script.js
+++ js/script.js
@@ -7,12 +7,27 @@ function myFunction() {
     }
 }
 
-
-
-
 $(document).ready(function(){
+  $("#collection").hover(function(){
+    $(this).html("A collection");
+  },function(){
+    $(this).html("A");
+  });
+
+  $("#doc").hover(function(){
+    $(this).html("documentation");
+  },function(){
+    $(this).html("d");
+  });
+
+  $("#about").hover(function(){
+    $(this).html("about");
+  },function(){
+    $(this).html("a");
+  });
   // Get the button that opens the modal
   var btn = $(".modalimg");
+  var homeImage = $("#homeImage");
 
   // Get the <span> element that closes the modal
   var span = $(".close");
@@ -24,17 +39,20 @@ $(document).ready(function(){
     thismodal += "modal"
     themodal = $("#"+thismodal);
     themodal.show();
+    homeImage.hide();
   })
 
   // When the user clicks on <span> (x), close the modal
   span.click(function() {
     themodal.hide();
+    homeImage.show();
   })
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
       if (event.target.className == "modalBox") {
           themodal.hide();
+          homeImage.show();
       }
   }
 })
