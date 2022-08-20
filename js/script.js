012e2961d6a083d88d8a92e4a74d2e7169f25712
AdaZhao1211 <adazhao1211@gmail.com>
2022-08-20 23:17:51 +0800

    topnave add name

--- js/script.js
+++ js/script.js
@@ -1,62 +1,7 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
 
 $(document).ready(function(){
-  $("#collection").hover(function(){
-    $(this).html("A portfolio");
-  },function(){
-    $(this).html("A");
-  });
-
 
   $("#myTopnav").load("../../header.html");
   // $("#footer").load("footer.html");
 
-  $("#doc").hover(function(){
-    $(this).html("documentation");
-  },function(){
-    $(this).html("d");
-  });
-
-  $("#about").hover(function(){
-    $(this).html("about");
-  },function(){
-    $(this).html("a");
-  });
-  // Get the button that opens the modal
-  var btn = $(".modalimg");
-  var homeImage = $("#homeImage");
-
-  // Get the <span> element that closes the modal
-  var span = $(".close");
-  var thismodal;
-  var themodal;
-  // When the user clicks the button, open the modal
-  btn.click(function() {
-    thismodal = btn.attr("id");
-    thismodal += "modal"
-    themodal = $("#"+thismodal);
-    themodal.show();
-    homeImage.hide();
-  })
-
-  // When the user clicks on <span> (x), close the modal
-  span.click(function() {
-    themodal.hide();
-    homeImage.show();
-  })
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-      if (event.target.className == "modalBox") {
-          themodal.hide();
-          homeImage.show();
-      }
-  }
 })
