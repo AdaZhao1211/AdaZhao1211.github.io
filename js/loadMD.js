ef6ee358b29b6c8fc3d816fe711940533c7d63e5
AdaZhao1211 <adazhao1211@gmail.com>
2022-08-21 00:09:23 +0800

    top and footer

---
+++ js/loadMD.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+  var converter = new showdown.Converter();
+  console.log(converter.makeHtml("#hello"));
+}
--- js/script.js
+++ js/script.js
@@ -1,7 +1,49 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
 $(document).ready(function(){
 
   $("#myTopnav").load("../../header.html");
+  $(".footer").load("../../footer.html");
+
   // $("#footer").load("footer.html");
 
+
+
+  // Get the button that opens the modal
+  var btn = $(".modalimg");
+  var homeImage = $("#homeImage");
+
+  // Get the <span> element that closes the modal
+  var span = $(".close");
+  var thismodal;
+  var themodal;
+  // When the user clicks the button, open the modal
+  btn.click(function() {
+    thismodal = btn.attr("id");
+    thismodal += "modal"
+    themodal = $("#"+thismodal);
+    themodal.show();
+    homeImage.hide();
+  })
+
+  // When the user clicks on <span> (x), close the modal
+  span.click(function() {
+    themodal.hide();
+    homeImage.show();
+  })
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target.className == "modalBox") {
+          themodal.hide();
+          homeImage.show();
+      }
+  }
 })
